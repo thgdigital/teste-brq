c8e2040c670051d1b54e471461333a680c3a01a9
Thiago <t.vaz.santos@gmail.com>
2019-11-21 20:28:10 -0300

    Criando projeto

---
+++ CarsShopping/Modulos/HomeList/HomeListView.swift
@@ -0,0 +1,52 @@
+//
+//  HomeListView.swift
+//  CarsShopping
+//
+//  Created by Thiago Vaz on 21/11/19.
+//  Copyright © 2019 Thiago Vaz. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class HomeListView: UICollectionViewController {
+    
+    var presenter: HomeListInput?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter?.fetchCars()
+        collectionView.backgroundColor = .white
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+    }
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+
+
+}
+extension HomeListView: HomeListOutput {
+    
+}
---
+++ CarsShopping/Modulos/HomeList/HomeProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  HomeProtocol.swift
+//  CarsShopping
+//
+//  Created by Thiago Vaz on 21/11/19.
+//  Copyright © 2019 Thiago Vaz. All rights reserved.
+//
+
+import Foundation
+
+protocol HomeListInput {
+    func fetchCars()
+}
+
+protocol HomeListOutput: class {
+    
+}
