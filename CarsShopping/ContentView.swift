c4575672689e206133a9c1acf54bb3f89e661846
Thiago <t.vaz.santos@gmail.com>
2019-11-21 13:43:25 -0300

    Initial Commit

---
+++ CarsShopping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarsShopping
+//
+//  Created by Thiago Vaz on 21/11/19.
+//  Copyright © 2019 Thiago Vaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
